0fbfcd218210cbe4050b6edd041ec8a1d4cfeac2
skyaaa123 <1143368663@qq.com>
2018-04-12 19:52:22 +0800

    修改以下

--- src/main/Java/Test.java
+++ src/main/Java/Test.java
@@ -3,6 +3,8 @@
  */
 public class Test {
     public static void main(String []args) {
-            System.out.println("hello01");
+
+        System.out.println("hello01");
+        System.out.println("hello02");
     }
 }
