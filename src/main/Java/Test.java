265fe8d5de33f84549c15f1fc391723b99fe554f
skyaaa123 <1143368663@qq.com>
2018-04-12 19:58:25 +0800

    再改一次

--- src/main/Java/Test.java
+++ src/main/Java/Test.java
@@ -4,5 +4,7 @@
 public class Test {
     public static void main(String []args) {
             System.out.println("hello01");
+            System.out.println("hello05");
+            System.out.println("hello08");
     }
 }
